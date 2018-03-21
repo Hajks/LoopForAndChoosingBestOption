44a3df6ad9518aa43c6f41aa3064b2b18a784aab
Hajk <tunczorx@gmail.com>
2018-03-21 12:26:54 +0100

    Loop for, saving for future.

--- LoopForAndChoosingBestOption/Form1.cs
+++ LoopForAndChoosingBestOption/Form1.cs
@@ -13,8 +13,32 @@ namespace LoopForAndChoosingBestOption
     public partial class Form1 : Form
     {
         public Form1()
+            // I shoudnt do this task in Visual Studio, but for as beginner it was so smart solution that i will write it because it might be usufull for me in future.
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Elephant[] elephants = new Elephant[7];
+            elephants[0] = new Elephant { Name = "Lloyd", EarSize = 40 };
+            elephants[1] = new Elephant { Name = "Kamil", EarSize = 33};
+            elephants[2] = new Elephant { Name = "Darek", EarSize = 42};
+            elephants[3] = new Elephant { Name = "Bartek", EarSize = 32 };
+            elephants[4] = new Elephant { Name = "Sławek", EarSize = 44 };
+            elephants[5] = new Elephant { Name = "Ziutek", EarSize = 37 };
+            elephants[6] = new Elephant { Name = "Wariat", EarSize = 45 };
+
+            Elephant biggestEars = elephants[0];
+            for (int i = 0; i < elephants.Length; i++)
+            {
+                if (elephants[i].EarSize > biggestEars.EarSize)
+                {
+                    biggestEars = elephants[i]; //Loop for should finally choose an elephant with biggest ears. 
+                }
+
+            }
+            MessageBox.Show("Największe uszy ma: " + biggestEars.Name + " i mają one " + biggestEars.EarSize.ToString() + " cm.");
         }
     }
 }
